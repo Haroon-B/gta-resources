e43fcd2f27cd5b6a4784c8670d1721b017dac754
Rikiborn <64733239+Rikiborn@users.noreply.github.com>
2020-07-21 17:15:49 +0300

    Fix (#336)
    * Fix
    
    Now works with (v1-Final ESX)
    
    * Update death.lua

--- disc-death/server/death.lua
+++ disc-death/server/death.lua
@@ -1,16 +1,18 @@
 RegisterServerEvent('disc-death:setDead')
 AddEventHandler('disc-death:setDead', function(isDead)
     local player = ESX.GetPlayerFromId(source)
+    
     MySQL.Async.execute('UPDATE users SET is_dead = @isDead WHERE identifier = @identifier', {
         ['@isDead'] = isDead,
-        ['@identifier'] = player.identifier
+        ['@identifier'] = player.getIdentifier()
     })
 end)
 
 ESX.RegisterServerCallback('disc-death:getDead', function(source, cb)
     local player = ESX.GetPlayerFromId(source)
+
     MySQL.Async.fetchAll('SELECT is_dead FROM users WHERE identifier = @identifier', {
-        ['@identifier'] = player.identifier
+        ['@identifier'] = player.getIdentifier()
     }, function(results)
         cb(results[1].is_dead)
     end)
@@ -18,8 +20,9 @@ end)
 
 ESX.RegisterServerCallback('disc-death:isPlayerDead', function(source, cb, target)
     local player = ESX.GetPlayerFromId(target)
+
     MySQL.Async.fetchAll('SELECT is_dead FROM users WHERE identifier = @identifier', {
-        ['@identifier'] = player.identifier
+        ['@identifier'] = player.getIdentifier()
     }, function(results)
         cb(results[1].is_dead)
     end)
